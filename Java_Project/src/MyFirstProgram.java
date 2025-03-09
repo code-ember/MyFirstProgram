4efa83f0f49c6ea95ed9822271194103623f8e5f
Ember <69181747+code-ember@users.noreply.github.com>
2025-03-08 19:50:09 -0600

    Add files via upload

---
+++ Java_Project/src/MyFirstProgram.java
@@ -0,0 +1,10 @@
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+
+        System.out.println("Let's change the code now...");
+        System.out.println("it's what I do!");
+
+    }
+
+}
